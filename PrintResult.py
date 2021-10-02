34c7ff0294ed4c61698c1caf0bad78302effd104
Daniel Spengler <danyu.spengler@gmail.com>
2021-10-02 15:22:43 +0200

    Added functionality to generate result files

---
+++ PrintResult.py
@@ -0,0 +1,33 @@
+"""
+Print results into files
+"""
+
+import logging
+import os
+import errno
+
+logger = logging.getLogger(__name__)
+
+def dump_text_to_file(path, name, extension, data):
+    """
+    Dumps given data into file with the structure: results/name
+    """
+    logger.info("Starting dump to new directory")
+    #filename = f"results/{name}"
+    #check if directory exists
+    filename = f"{name}.{extension}"
+    logger.info("Filepath: %s", filename)
+    i = 0
+    while os.path.exists(f"{path}/{filename}"):
+        logger.info("File already exists.")
+        logger.info("Adding number to it")
+        i += 1
+        filename = f"{name}{i}.{extension}"
+
+    filename = f"{path}/{filename}"
+
+    #dump data to file
+    with open(filename, "w") as outfile:
+        logger.info("Creating file: %s", outfile.name)
+        outfile.write(data)
+        logger.info("Dumped data into output file %s", outfile.name)
--- StartAbstractGenerator.py
+++ StartAbstractGenerator.py
@@ -4,10 +4,13 @@ Handle the general workflow of the Abstract generation
 
 from logging.config import fileConfig
 import logging
+import PrintResult
 
-logging.config.fileConfig('logging_config.ini', disable_existing_loggers=False)
+logging.config.fileConfig('config/logging_config.ini', disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
+RESULT_PATH = "results"
+FILE_EXTENSION = "txt"
 
 def main():
     #load text into memory
@@ -19,7 +22,10 @@ def main():
     #generate abstract
 
     #print abstract to file
-    
+    filename = "generated"
+    data = "Hello World"
+    PrintResult.dump_text_to_file(RESULT_PATH, filename, FILE_EXTENSION, data)
+
     return
 
 
