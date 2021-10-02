e97879f6fb3aa6405cfeaf57b79904f9418bab6d
Daniel Spengler <danyu.spengler@gmail.com>
2021-10-02 12:58:45 +0200

    Grober Aufbau des Ablaufs

---
+++ StartAbstractGenerator.py
@@ -0,0 +1,27 @@
+"""
+Handle the general workflow of the Abstract generation
+"""
+
+from logging.config import fileConfig
+import logging
+
+logging.config.fileConfig('logging_config.ini', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+
+def main():
+    #load text into memory
+
+    #setup transformer
+
+    #run model
+
+    #generate abstract
+
+    #print abstract to file
+    
+    return
+
+
+if __name__ == '__main__':
+    main()
