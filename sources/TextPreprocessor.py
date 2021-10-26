a04b5d8dc23beb94a310364f3e3bc91ad13ff717
Daniel Spengler <danyu.spengler@gmail.com>
2021-10-26 23:43:58 +0200

    Added first preprocessing for cleanup

--- sources/TextPreprocessor.py
+++ sources/TextPreprocessor.py
@@ -6,8 +6,26 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+REFERENCES = 'REFERENCES'
+INTRODUCTION = 'INTRODUCTION'
 
-def preprocess_text(raw_data):
+def preprocess_text(raw_data: str):
     logger.info("Starting text preprocessing...")
-    clean_text = raw_data.strip()
+    clean_text = raw_data
+
+    logger.info("Checking for introduction...")
+    if INTRODUCTION in clean_text:
+        logger.info("Removing text before introduction")
+        clean_text = ''.join(clean_text.partition(INTRODUCTION)[1:])
+
+    logger.info(f"Checking for references chapter...")
+    if REFERENCES in clean_text:
+        logger.info("Removing references...")
+        clean_text = clean_text.partition(REFERENCES)[0]
+
+    logger.info("Removing leading and trailing whitespaces...")
+    clean_text = clean_text.strip()
+
+    
+    logger.info("Done preprocessing")
     return clean_text
